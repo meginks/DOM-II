b89069a085284d36030c15249a11991bd28d2c1d
unknown <38929577+meginks@users.noreply.github.com>
2019-01-29 15:57:44 -0600

    have 11 unique event listeners

--- js/index.js
+++ js/index.js
@@ -10,8 +10,10 @@ let boatPic = document.getElementById("boat-photo");
 let headerBar = document.querySelectorAll(".main-navigation .container .nav-container");
 let funInSun = document.getElementById("fun-in-sun");
 let logoHeading = document.querySelector(".logo-heading");
-// click - changes first signup button to green on click
+let adventurePic = document.getElementById("adventure-img");
+let funPic = document.getElementById("fun-img");
 
+// click - changes first signup button to green on click
 
 signupBtn1.addEventListener('click', event => {signupBtn1.style.backgroundColor = "green";
 signupBtn1.style.color = "white";});
@@ -53,10 +55,29 @@ boatPic.addEventListener('dragstart', event => {
 // mouseenter -- going onto logo changes text 
 
 logoHeading.addEventListener('mouseenter', event => {
-logoHeading.innerHTML = "UN-FUN BUS"});
+logoHeading.innerHTML = "GOATS TAKE OVER"});
 
 // mouseleave -- going off the logo changes text back
 
 logoHeading.addEventListener('mouseleave', event => {
-    logoHeading.innerHTML = "Fun Bus"
+    logoHeading.innerHTML = "Goats are here to stay. MWAHAHA"
+});
+
+// mousemove -- goat picture pops up on fun bus image
+
+funBusPic.addEventListener('mousemove', event => {
+funBusPic.setAttribute("src", "img/goat-in-sweater.jpg")});
+
+
+// mousedown -- when mousedown, changes to baby goat pic
+
+boatPic.addEventListener('mousedown', event => {
+    boatPic.setAttribute("src", "img/babygoat.jpg")
+});
+
+
+// contextmenu -- when right click, image changes to goat
+
+funPic.addEventListener('contextmenu', event => {
+    funPic.setAttribute("src", "img/goathead.jpg")
 });
